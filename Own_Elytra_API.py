ae2ee2f606e866e79cf3d0adf54d37e402abfdc1
Don <kimaegaii@gmail.com>
2024-03-16 21:00:23 +0000

    Update Own_Elytra_API.py

--- Own_Elytra_API.py
+++ Own_Elytra_API.py
@@ -2,11 +2,15 @@ from flask import Flask, jsonify
 import sqlite3
 
 app = Flask(__name__)
-
+    
 def get_db_connection():
     conn = sqlite3.connect('elytra_prices.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @app.route('/prices', methods=['GET'])
 def prices():
